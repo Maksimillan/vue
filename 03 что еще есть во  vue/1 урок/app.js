af66e4b6f6c52a095a920124dafa7e8298137ee9
Maksimillian <ismatilaevmuzaffar6@gmail.com>
2022-07-10 18:01:27 +0300

    vue

--- 03 что еще есть во  vue/1 урок/app.js
+++ 03 что еще есть во  vue/1 урок/app.js
@@ -2,10 +2,14 @@ Vue.createApp({
 
     data() {
         return {
-         title:'Я есть Груд',
-         myhtml:'<h1> Vue 3 app<h1/>'
-
-
+            title: 'Я есть Груд',
+            myhtml: '<h1> Vue 3 app<h1/>',
+            person: {
+                firstName:'Muzaffar',
+                lastName:'Ismatillayev',
+                age:22,
+            },
+            items:[1,2,3,4,5,6,7,8,9],
         }
     }
 }).mount('#app')
